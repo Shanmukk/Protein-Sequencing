a3c63e735154f5debc5bf743316df52794d4c8e9
Shanmukk <vemulakondashanmuk@msitprogram.net>
2022-04-18 12:22:09 +0530

    completed

--- hw6_protein.py
+++ hw6_protein.py
@@ -130,7 +130,11 @@ Parameters: 2D list of strs
 Returns: list of strs
 '''
 def combineProteins(proteinList):
-    return
+    lst = []
+    for i in proteinList:
+        for j in i:
+            lst.append(j)
+    return lst
 
 
 '''
